a0603aef60e8d29847a02def7217b98ba4bc5fe3
Hédie <hedie.strokes@gmail.com>
2021-08-23 15:37:25 -0300

    Added MyScoreApplication and Modules file

---
+++ app/src/main/java/com/example/myscore/MyScoreApplication.kt
@@ -0,0 +1,17 @@
+package com.example.myscore
+
+import android.app.Application
+import com.example.myscore.injection.Modules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MyScoreApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@MyScoreApplication)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myscore/injection/Modules.kt
@@ -0,0 +1,6 @@
+package com.example.myscore.injection
+
+
+object Modules {
+
+}
