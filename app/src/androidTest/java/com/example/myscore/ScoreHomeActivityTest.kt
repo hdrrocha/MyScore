622d5de27241f3e7a9233359f1c9b1a039400dcf
Hédie <hedie.strokes@gmail.com>
2021-08-24 10:55:48 -0300

    adjusted UI test nomenclature

--- app/src/androidTest/java/com/example/myscore/ScoreHomeActivityTest.kt
+++ app/src/androidTest/java/com/example/myscore/ScoreHomeActivityTest.kt
@@ -6,17 +6,40 @@ import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.example.myscore.ui.ScoreHomeActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ScoreHomeActivityTest {
+    @get:Rule
+    val rule = ActivityTestRule(ScoreHomeActivity::class.java, true, true)
 
     @Test
-    fun test_ShouldShowLoadingWhenOpenActivity() {
-        onView(withId(R.id.loadingScore)).isVisible()
-        onView(withId(R.id.loadingScore)).isGone()
+    fun  when_create_activity_it_should_display_loadingScore() {
+        try {
+            Thread.sleep(2000)
+            onView(withId(R.id.loadingScore)).isVisible()
+            onView(withId(R.id.loadingScore)).isGone()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    @Test
+    fun  when_end_loadingScoreactivity_it_should_display_progressScore() {
+        try {
+            Thread.sleep(2000)
+            onView(withId(R.id.loadingScore)).isVisible()
+            onView(withId(R.id.loadingScore)).isGone()
+            onView(withId(R.id.progressScore)).isVisible()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 
     fun ViewInteraction.isGone() = getViewAssertion(ViewMatchers.Visibility.GONE)
