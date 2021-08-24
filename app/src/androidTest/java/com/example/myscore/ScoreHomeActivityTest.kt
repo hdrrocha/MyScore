ebec6353d2bd503ccb7e2b513098b9981f6e6d77
Hédie <hedie.strokes@gmail.com>
2021-08-24 01:03:50 -0300

    Added ScoreHomeActivityTest

---
+++ app/src/androidTest/java/com/example/myscore/ScoreHomeActivityTest.kt
@@ -0,0 +1,31 @@
+package com.example.myscore
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ScoreHomeActivityTest {
+
+    @Test
+    fun test_ShouldShowLoadingWhenOpenActivity() {
+        onView(withId(R.id.loadingScore)).isVisible()
+        onView(withId(R.id.loadingScore)).isGone()
+    }
+
+    fun ViewInteraction.isGone() = getViewAssertion(ViewMatchers.Visibility.GONE)
+
+    fun ViewInteraction.isVisible() = getViewAssertion(ViewMatchers.Visibility.VISIBLE)
+
+    fun ViewInteraction.isInvisible() = getViewAssertion(ViewMatchers.Visibility.INVISIBLE)
+
+    private fun getViewAssertion(visibility: ViewMatchers.Visibility): ViewAssertion? {
+        return ViewAssertions.matches(ViewMatchers.withEffectiveVisibility(visibility))
+    }
+}
